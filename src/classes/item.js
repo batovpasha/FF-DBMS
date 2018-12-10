a06790fde9658035b3bfd021523e389a4d6998b3
batovpasha <batovpasha@gmail.com>
2018-12-10 15:54:21 +0200

    Some code refactoring

--- src/classes/item.js
+++ src/classes/item.js
@@ -1,45 +1,43 @@
 'use strict';
 
 class ItemScheme { // a class that contains a schema of user`s objects
-  constructor(arrayOfUserFields){
-    this.userFields = new Array();
-    for (let i of arrayOfUserFields){
-      this.userFields.push(i);
-    }
+  constructor(arrayOfUserFields) {
+    this.userFields = arrayOfUserFields.slice();
   }
-  validityCheck(userObject){ // function to check the pattern match user`s object
-    let result = true;
-    for (let i in userObject){
-      result = result && (this.userFields.indexOf(i) == -1 ? false : true);
-    }
+  
+  validityCheck(userObject) { // function to check the pattern match user`s object
+    // validity reducer
+    const reducer = (acc, key) => acc && this.arrayOfUserFields.includes(key);
+
+    let result = Object.keys(userObject).reduce(reducer, true);
     return result;
   }
 };
 
 class Item { // a class that contains a user`s object, its hash and id
-  constructor(userObject){
+  constructor(userObject) {
     this.hash = 0;
     this.id = 0;
-    this.item = userObject;
+    this.item = Object.assign({}, userObject);
   }
 
-  setHash(hash){
+  set hash(hash) {
     this.hash = hash;
   }
 
-  getHash(){
+  get hash() {
     return this.hash;
   }
 
-  setId(id){
+  set id(id) {
     this.id = id;
   }
 
-  getId(){
+  get id() {
     return this.id;
   }
 
-  getItem(){
+  get item() {
     return this.item;
   }
 }
