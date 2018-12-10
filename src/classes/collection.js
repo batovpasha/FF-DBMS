48c7b98819372689c1147df5c79293b100cb86b5
CodeGangsta44 <dovroman44@gmail.com>
2018-12-10 17:12:20 +0200

    Collection implementation added

---
+++ src/classes/collection.js
@@ -0,0 +1,69 @@
+const ItemSchema = require('./item.js').ItemSchema;
+const Item = require('./item.js').Item;
+const crypto = require('crypto');
+
+class Collection{ // a class that describes the structure and behavior of the collection
+  constructor(itemSchema, keySchema){
+    this.itemSchema = new ItemSchema(itemSchema);
+    this.keySchema = keySchema;
+    this.hashTable = new Map();
+    this.numberOfItems = 0;
+  }
+
+  hashFunction(key, password){ // a function that generates a hash value for a given key
+    const cipher = crypto.createCipher('aes192', password);
+    let hash = cipher.update(key, 'utf8', 'hex');
+    hash += cipher.final('hex');
+    return hash;
+  }
+
+  insertItem(item, password){ // method of inserting an element into a hash table
+    if (this.itemSchema.validityCheck(item)){
+      let key = '';
+      for (let i of this.keySchema) key += item[i].toString();
+      let hash = this.hashFunction(key, password);
+      this.hashTable.set(hash, new Item(hash, ++this.numberOfItems, item));
+    }
+  }
+
+  findItem(query, password){ // element by key search method
+    let key = '';
+    for (let i of this.keySchema) key += item[i].toString();
+    let hash = this.hashFunction(key, password);
+    return this.hashTable.get(hash).item;
+  }
+
+  updateItem(item, password){ // method of updating the value according to the given key
+    if (this.itemSchema.validityCheck(item)){
+      let queryItem = this.findItem(item, password);
+      for (let i in item) queryItem[i] = item[i];
+    }
+  }
+
+  drop(password){ // method of cleaning the collection
+    this.hashTable.clear();
+  }
+
+};
+
+let col = new Collection(['name', 'surname', 'age'], ['name', 'surname']);
+
+let item = {
+  name: 'Peter',
+  surname: 'Digger',
+  age: 23
+}
+
+col.insertItem(item, 'password');
+
+console.dir(col.findItem(item, 'password'));
+
+col.updateItem(item, 'password');
+
+console.dir(col.findItem(item, 'password'));
+
+console.dir(col);
+
+col.drop();
+
+console.dir(col);
--- src/classes/item.js
+++ src/classes/item.js
@@ -1,13 +1,13 @@
 'use strict';
 
-class ItemScheme { // a class that contains a schema of user`s objects
+class ItemSchema { // a class that contains a schema of user`s objects
   constructor(arrayOfUserFields) {
     this.userFields = arrayOfUserFields.slice();
   }
-  
+
   validityCheck(userObject) { // function to check the pattern match user`s object
     // validity reducer
-    const reducer = (acc, key) => acc && this.arrayOfUserFields.includes(key);
+    const reducer = (acc, key) => acc && this.userFields.includes(key);
 
     let result = Object.keys(userObject).reduce(reducer, true);
     return result;
@@ -15,29 +15,38 @@ class ItemScheme { // a class that contains a schema of user`s objects
 };
 
 class Item { // a class that contains a user`s object, its hash and id
-  constructor(userObject) {
-    this.hash = 0;
-    this.id = 0;
-    this.item = Object.assign({}, userObject);
+  constructor(hash, id, userObject) {
+    this._hash = hash;
+    this._id = id;
+    this._item = Object.assign({}, userObject);
   }
 
   set hash(hash) {
-    this.hash = hash;
+    this._hash = hash;
   }
 
   get hash() {
-    return this.hash;
+    return this._hash;
   }
 
   set id(id) {
-    this.id = id;
+    this._id = id;
   }
 
   get id() {
-    return this.id;
+    return this._id;
+  }
+
+  set item(item) {
+    this._item = item;
   }
 
   get item() {
-    return this.item;
+    return this._item;
   }
+}
+
+module.exports = {
+  ItemSchema,
+  Item
 }
