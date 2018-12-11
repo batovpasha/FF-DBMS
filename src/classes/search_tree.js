c8e3d6f4ac643e27f5012af51c37c9983d87003a
CodeGangsta44 <dovroman44@gmail.com>
2018-12-12 01:35:13 +0200

    Implementation of searching items in tree added

--- src/classes/search_tree.js
+++ src/classes/search_tree.js
@@ -11,41 +11,40 @@ class Node{
   }
 }
 
-class SearchTree{
+class SearchTree{ // a class that describes the structure and implementation of the multi-level binary search tree
 
   constructor(arrayOfFields){
     this.arrayOfFields = arrayOfFields;
     this.root = undefined;
   }
 
-  switchBranch(node, branch, item, type){
+  switchBranch(node, branch, item, type){ // method to switch to another branch when inserting element
     if (node[branch] == undefined) node[branch] = new Node(type, item[type], item);
-    else this.addNode(node[branch], item);
+    else this.insertNode(node[branch], item);
   }
 
-  switchTree(node, type, item){
+  switchTree(node, type, item){ // method to switch to another tree(alternative) when inserting element
 
     if (node.alternativeTree == undefined){
 
-      let currIndex = this.arrayOfFields.indexOf(node.type);
-      let nextType = this.arrayOfFields[currIndex + 1];
+      let nextType = this.arrayOfFields[this.arrayOfFields.indexOf(node.type) + 1];
 
       if(nextType != undefined){
         node.alternativeTree = new Node(nextType, node.value[nextType], node.value);
-        this.addNode(node.alternativeTree, item);
+        this.insertNode(node.alternativeTree, item);
       }
 
-    } else this.addNode(node.alternativeTree, item);
+    } else this.insertNode(node.alternativeTree, item);
 
   }
 
-  createRoot(item){
+  createRoot(item){ // root initialization method when inserting the first element
     let type = this.arrayOfFields[0];
     let key = item[type];
     this.root = new Node(type, key, item);
   }
 
-  addNode(...args){
+  insertNode(...args){  // method of adding a node to a tree
 
     if(args.length == 2){
 
@@ -69,46 +68,93 @@ class SearchTree{
 
       if (this.root == undefined) this.createRoot(item);
 
-      else this.addNode(this.root, item);
+      else this.insertNode(this.root, item);
 
     }
+  } // method of adding a node to a tree
+
+  searchItem(...args){ // method for searching for items in tree (query handler)
+
+    if(args.length == 3){
+
+      let node = args[0];
+      let item = Object.assign({}, args[1]);;
+      let resultArray = args[2];
+      let type = node.type;
+
+      if (type in item){
+
+        if(item[type] == node.key){
+          delete item[type];
+          if (node.alternativeTree) this.searchItem(node.alternativeTree, item, resultArray);
+          else resultArray.push(node.value);
+        }
+
+        if(item[type] > node.key && node.right){
+          this.searchItem(node.right, item, resultArray);
+        }
+
+        if(item[type] < node.key && node.left){
+          this.searchItem(node.left, item, resultArray);
+        }
+
+      } else {
+
+        let res = true;
+        for (let i in item) res = res && item[i] == node.value[i];
+        if (res && !node.alternativeTree) resultArray.push(node.value);
+
+        if(node.alternativeTree) this.searchItem(node.alternativeTree, item, resultArray);
+        if(node.right) this.searchItem(node.right, item, resultArray);
+        if(node.left) this.searchItem(node.left, item, resultArray);
+
+      }
+
+    }
+
+    if(args.length == 2) this.searchItem(this.root, args[0], args[1]);
+
   }
+
 };
 
-let item1 = {
-  name: 'Peter',
-  surname: 'Digger',
-  age: 23
-}
+/* ---EXAMPLES--- */
 
-
-let item2 = {
-  name: 'Dick',
-  surname: 'Digger',
-  age: 23
-}
-
-let item3 = {
-  name: 'Nick',
-  surname: 'zigger',
-  age: 23
-}
-
-let item4 = {
-  name: 'Dick',
-  surname: 'zigger',
-  age: 25
-}
-
-let tree = new SearchTree(['name', 'surname', 'age']);
-
-//console.dir(tree);
-
-tree.addNode(item1);
-tree.addNode(item2);
-tree.addNode(item3);
-tree.addNode(item4);
-tree.addNode(item4);
-
-console.dir(tree);
-console.log(JSON.stringify(tree));
+// let item1 = {
+//   name: 'Peter',
+//   surname: 'Digger',
+//   age: 23
+// }
+//
+//
+// let item2 = {
+//   name: 'Dick',
+//   surname: 'Digger',
+//   age: 23
+// }
+//
+// let item3 = {
+//   name: 'Nick',
+//   surname: 'zigger',
+//   age: 23
+// }
+//
+// let item4 = {
+//   name: 'Dick',
+//   surname: 'zigger',
+//   age: 25
+// }
+//
+// let tree = new SearchTree(['name', 'surname', 'age']);
+//
+// tree.insertNode(item1);
+// tree.insertNode(item2);
+// tree.insertNode(item3);
+// tree.insertNode(item4);
+//
+// let result = [];
+//
+// let searchQuery = {surname: 'zigger'};
+//
+// tree.searchItem(searchQuery, result)
+// console.dir(result);
