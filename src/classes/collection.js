86ba9e6e910fbfa9bd28b838f1419b575b84dfe2
batovpasha <batovpasha@gmail.com>
2018-12-13 22:02:29 +0200

    Rewrote 'for' loop to 'reduce'

--- src/classes/collection.js
+++ src/classes/collection.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const ItemSchema = require('./item.js').ItemSchema;
 const Item = require('./item.js').Item;
 const HashSpace = require('./hash-space.js').HashSpace;
@@ -9,50 +11,48 @@ class Collection { // a class that describes the structure and behavior of the c
   constructor(itemSchema, keySchema, typeOfStruct) {
     this.itemSchema = new ItemSchema(itemSchema);
     this.keySchema = keySchema;
+    this.typeOfStruct = typeOfStruct;
     this.hashTable = new Map();
     this.numberOfItems = 0;
-    this.typeOfStruct = typeOfStruct;
 
-    if (typeOfStruct === 1) this.searchStructure = new HashSpace(this.itemSchema);
-    if (typeOfStruct === 2) this.searchStructure = new SearchTree(this.itemSchema);
+    if (typeOfStruct === 1)
+      this.searchStructure = new HashSpace(this.itemSchema);
+    if (typeOfStruct === 2) 
+      this.searchStructure = new SearchTree(this.itemSchema);
   }
 
   createHash(key, password) { // a function that generates a hash value for a given key
     const cipher = crypto.createCipher('aes192', password);
 
-    let hash = cipher.update(key, 'utf8', 'hex') + cipher.final('hex');
+    const hash = cipher.update(key, 'utf8', 'hex') + cipher.final('hex');
     return hash;
   }
 
   insert(item, password) { // method of inserting an element into a hash table
     if (this.itemSchema.validityCheck(item)) {
-      let key = '';
-
-      this.keySchema.forEach(i => key += item[i].toString());
-
-      let hash = this.createHash(key, password);
+      const key = this.keySchema.reduce((acc, val) => acc += item[val], '');
+      const hash = this.createHash(key, password);
+      
       this.hashTable.set(hash, new Item(hash, ++this.numberOfItems, item));
-
       this.searchStructure.insert(item);
     }
   }
 
   findOne(query, password) { // element by key search method
     if (this.itemSchema.validityCheck(query)) {
-      let key = '';
-
-      for (let i of this.keySchema)
-        key += item[i].toString();
-
-      let hash = this.createHash(key, password);
+      const key = this.keySchema.reduce((acc, val) => acc += query[val], '');
+      const hash = this.createHash(key, password);
+      
       return this.hashTable.get(hash).item;
     } else console.log("Incorrect item schema!");
   }
 
-  find(query){
+  find(query) {
     if (this.itemSchema.validityCheck(query)) {
       return this.searchStructure.find(query);
-    } else console.log("Incorrect item schema!");
+    } else {
+      console.log("Incorrect item schema!")
+    };
   }
 
   updateItem(item, password) { // method of updating the value according to the given key
@@ -65,35 +65,36 @@ class Collection { // a class that describes the structure and behavior of the c
 
   drop() { // method of cleaning the collection
     this.hashTable.clear();
-    if (this.typeOfStruct === 1) this.searchStructure = new HashSpace(this.itemSchema);
-    if (this.typeOfStruct === 2) this.searchStructure = new SearchTree(this.itemSchema);
+
+    if (this.typeOfStruct === 1) 
+      this.searchStructure = new HashSpace(this.itemSchema);
+    if (this.typeOfStruct === 2) 
+      this.searchStructure = new SearchTree(this.itemSchema);
   }
 };
 
 /* ---EXAMPLES--- */
 
 // let col = new Collection(['name', 'surname', 'age'], ['name', 'surname'], 1);
-//
+
 // let item = {
 //   name: 'Peter',
 //   surname: 'Digger',
 //   age: 23
-// }
-//
+// };
+
 // col.insert(item, 'password');
-//
+
 // console.dir(col.findOne(item, 'password'));
 // console.dir(col.find({surname: 'Digger'}));
 // console.dir(col.find({surname: 'Diger'}));
-//
-//
-//
+
 // console.dir(col);
-//
+
 // col.drop();
-//
+
 // console.dir(col);
 
 module.exports = {
   Collection
-}
+};
