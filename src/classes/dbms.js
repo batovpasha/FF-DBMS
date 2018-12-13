d376b5fd63f1d7c2c0519b071f3969a78adc1fb4
batovpasha <batovpasha@gmail.com>
2018-12-14 00:28:54 +0200

    Added unfinished dbms implementation

---
+++ src/classes/dbms.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const DataBase = require('./db.js').DataBase;
+
+class DBMS {
+  constructor() {
+    this.identificationData = new Map();
+  }
+
+  connect(login, password) {
+    if (this.identificationData.has(login) && 
+        this.identificationData.get(login).password === password) 
+      return this;
+    
+    else if (this.identificationData.has(login) &&
+             this.identificationData.get(login).password !== password) {
+      console.log('Incorrect password!');
+      return null;
+    }
+
+    else {
+      this.identificationData.set(login, { password, databases: new Map() });
+      return this;
+    }
+  }
+
+  createDatabase(name, login, password) {
+    if (this.identificationData.has(login) && 
+        this.identificationData.get(login).password === password) {
+      const database = new DataBase(name);
+      this.identificationData.get(login).databases.set(name, database);
+    }
+    
+    else if (this.identificationData.has(login) && 
+             this.identificationData.get(login).password !== password) {
+      console.log('Incorrect password!');
+      return;
+    }
+
+    else return;
+  }
+
+  dropDatabase(name, login, password) {
+    if (this.identificationData.has(login) && 
+        this.identificationData.get(login).password === password) {
+      
+      if (this.identificationData.get(login).databases.has(name)) {
+        this.identificationData.get(login).databases.delete(name);
+        return;
+      }
+      
+      if (!this.identificationData.get(login).databases.has(name)) {
+        console.log(`This client has not ${name} database`);
+        return;
+      }
+    }
+    
+    else if (this.identificationData.has(login) && 
+             this.identificationData.get(login).password !== password) {
+      console.log('Incorrect password!');
+      return;
+    }
+
+    else return;    
+  }
+
+  find(query, database, collection, login, password) { // database and table names
+    if (this.identificationData.get(login) === password) {
+    
+    }
+  }
+
+  print() {
+    console.log(this.identificationData);
+  }
+};
+
+// let dbms = new DBMS();
+
+// dbms.connect('Pasha', '12345');
+// dbms.createDatabase('users', 'Pasha', '12345');
+// dbms.createDatabase('dsdas', 'Pasha', '12345');
+// dbms.print();
+
+
+// dbms.dropDatabase('users', 'Pasha', '12345')
+// dbms.print();
+
+// dbms.dropDatabase('dsdas', 'Pasha', '12345');
+// dbms.print();
---
+++ src/classes/test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let map = new Map();
+
+map.set('pasha', '998877oi');
+
+if (map.get('pasha') === '98877oi')
+  console.log('adsadas');
